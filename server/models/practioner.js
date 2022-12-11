e07bb65941b151f058ea17aacb0a6227f4231034
su-de-sh <sudesh7443@gmail.com>
2022-12-11 14:04:28 +0545

    feat: make end point for getting list of all practitioners

---
+++ server/models/practioner.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const practionerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  startTime: {
+    type: String,
+    required: true,
+  },
+  endTime: {
+    type: String,
+    required: true,
+  },
+});
+
+const Practioner = mongoose.model("Practioner", practionerSchema);
+
+module.exports = Practioner;
