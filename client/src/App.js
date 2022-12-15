2628dd8fb65d58cc82b59611da0e983e87dd379c
su-de-sh <sudesh7443@gmail.com>
2022-12-16 00:29:26 +0545

    feat: frontend initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./sections/Home";
+import Signin from "./sections/Signin";
+import Signup from "./sections/Signup";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
