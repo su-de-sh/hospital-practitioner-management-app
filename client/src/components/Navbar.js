20e0f9437b4c3e818ecb8ae955794169fa05506f
su-de-sh <sudesh7443@gmail.com>
2022-12-18 02:36:32 +0545

    chore: remove annoying warning

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,6 +11,7 @@ const Navbar = () => {
 
   useEffect(() => {
     dispatch(initializeUser());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleLogout = () => {
